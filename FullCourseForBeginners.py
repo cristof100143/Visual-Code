02cef9eba2f80b8617c5205e1bd4f16a93c0eab3
Cristo <crisbro888@yahoo.com>
2025-03-12 09:33:49 +1100

    Newest Final

--- FullCourseForBeginners.py
+++
@@ -1,7 +0,0 @@
-distance = input('What is the distance and unit the object travelled? ')
-distance.replace('distance', 'the')('acceleration', 'of')
-
-distance.remove[41:50]
-    print('Your object is moving ' + distance_input)
-else:
-    print('Error!')
